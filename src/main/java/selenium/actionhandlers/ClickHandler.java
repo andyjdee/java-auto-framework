2ce7866bc793f6487a84fbe5772a15a7151641ae
Andrew Demetriou <andrew.demetriou91@gmail.com>
2018-07-18 09:08:59 +0100

    Included initial implementation of selenium 'Action Handler' classes.

---
+++ src/main/java/selenium/actionhandlers/ClickHandler.java
@@ -0,0 +1,27 @@
+package selenium.actionhandlers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Created by Andrew Demetriou on 17/07/2018.
+ */
+public class ClickHandler {
+
+    private static WebDriver driver;
+
+    public ClickHandler(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void leftClick(WebElement element) {
+        element.click();
+    }
+
+    public void rightClick(WebElement element) {
+        Actions actions = new Actions(this.driver);
+        actions.contextClick(element);
+    }
+
+}
---
+++ src/main/java/selenium/actionhandlers/KeyHandler.java
@@ -0,0 +1,21 @@
+package selenium.actionhandlers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Andrew Demetriou on 17/07/2018.
+ */
+public class KeyHandler {
+
+    private static WebDriver driver;
+
+
+    public KeyHandler(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void sendText(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+}
